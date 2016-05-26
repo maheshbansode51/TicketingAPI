05d15d8c49cc418928e8eaf1ff3df56159ee142e
tavisca-mbansode <mbansode@tavisca.com>
2016-05-26 22:17:44 +0530

    Added Unity for DI

---
+++ TicketingAPI_V1/Repositories/IPlatformTicketRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketingAPI_V1.Models;
+
+namespace TicketingAPI_V1.Repositories
+{
+    public interface IPlatformTicketRepository
+    {
+        Task<BaseResult<DisplayPlatformTicketModel>> BookPlatformTicket(string userId, PlatformTicketModel model);
+    }
+}
